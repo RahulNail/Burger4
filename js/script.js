811fbebf88594a60763684edffabffdb1547c1cb
RahulNail <79205807+RahulNail@users.noreply.github.com>
2022-05-03 17:21:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let searchBtn = document.querySelector('#search-btn');
 let searchForm = document.querySelector('.header .search-form');
 
 if("serviceWorker" in navigator){
-   navigator.serviceWorker.register("https://rahulnail.github.io/Burger/sw.js")
+   navigator.serviceWorker.register("https://rahulnail.github.io/Burger4/sw.js")
    .then(()=>{
       console.log("sw registered")
    })
